3c1292b4770d18e848040d9cd0f3a58572a55e40
Kameron Ronald <khr675@gmail.com>
2022-09-24 18:27:16 -0500

    Updated test example file.

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 """
-This is an example test file. Copy the contents into a scratch file or move the file out of terminal-ui-alt folder to run.
-"""
+This is an example test file. Copy the contents into a file one level up in order to run.
 
-# from terminal-ui-alt import Screen
-#
-# if __name__ == '__main__':
-#     my_screen = Screen()
-#     my_screen.play()
+
+import terminalUI as ui
+
+my_screen = ui.Screen()
+my_page = ui.Page('my_page')
+print('creating text...')
+my_element = ui.Text('hello world.')
+
+
+my_page.add(my_element)
+my_screen.add_page(my_page)
+my_screen.play()
+"""
